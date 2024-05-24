dfeee139860361a3a2cd5d73fdec60c4b9b94c78
nex3z <litianxing9@gmail.com>
2024-05-24 14:18:36 +0800

    Update locations.

--- cc_net/__init__.py
+++ cc_net/__init__.py
@@ -3,3 +3,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 #
+from pathlib import Path
+
+
+MODULE_ROOT = Path(__file__).resolve().parent
+PROJECT_ROOT = MODULE_ROOT.parent
+DATA_ROOT = PROJECT_ROOT / 'data'
